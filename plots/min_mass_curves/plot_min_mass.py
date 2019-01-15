cbee37fa0d876637d3913d8f68029acad044c0ce
Alexander Swenson <aaswenson@wisc.edu>
2019-01-15 11:17:24 -0600

    final edits

--- plots/min_mass_curves/plot_min_mass.py
+++ plots/min_mass_curves/plot_min_mass.py
@@ -40,21 +40,20 @@ cr_data = load_data_crit('crit_mass.txt')
 
 
 fig = plt.figure()
-colors = ['k', 'g', 'b', 'c', 'r']
+colors = ['c', 'g', 'b', 'r', 'k']
 
 y1 = cr_data['mass']
-
-for i, Q in enumerate([sorted(th_data.keys())[-1]]):
+for i, Q in enumerate(sorted(th_data.keys())[1:]):
     dat = th_data[Q]
-    plt.scatter(dat['frac'], dat['mass'], label=str(int(Q)), s=8, c=colors[i])
+    plt.scatter(dat['frac'], dat['mass'], label=str(int(Q)) + ' [kW]', s=8, c=colors[i])
     plt.plot(dat['frac'], dat['mass'], c=colors[i], linestyle='--')
     y2 = dat['mass']
 
-plt.scatter(cr_data['frac'], cr_data['mass'], s=8, c='y', label='Critical Mass')
+plt.scatter(cr_data['frac'], cr_data['mass'], s=8, c='y', label='Critical Constraint')
 plt.plot(cr_data['frac'], cr_data['mass'], c='y', linestyle='--')
 plt.ylim((0, 2000))
 plt.xlabel('fuel fraction [-]')
 plt.ylabel('core mass [kg]')
 plt.title('Limiting Core Mass')
-plt.legend(title='Thermal Power [kW]')
+plt.legend(title='Limiting Case')
 plt.savefig('limiting_core_mass.png', dpi=700)
